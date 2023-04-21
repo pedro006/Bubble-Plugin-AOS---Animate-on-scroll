cf4932fff9a85328f56ffd66660a21a43da7fa34
pierre <47930395+pedro006@users.noreply.github.com>
2023-04-21 10:53:46 +0200

    Automatic commit from Bubble

---
+++ actions/AAT-850n8/client.js
@@ -0,0 +1,38 @@
+function(properties, context) {
+        
+    function setProps(target) {
+        target.dataset.aos = properties.animation;
+        target.dataset.aosEasing = properties.easing;
+        target.dataset.aosAnchorPlacement = properties.anchor;
+        target.dataset.aosDuration = properties.duration;
+        target.dataset.aosOnce = properties.once;
+        target.dataset.aosMirror = properties.mirror;
+        target.dataset.aosOffset = properties.offset;
+        target.dataset.aosDelay = properties.delay;
+        
+        // remove opacity default property
+        if (target.style.opacity) {
+        	target.style.removeProperty('opacity');
+        }
+    }
+        
+    var selectors = properties.selectors.trim().split(/\s*,\s*/);
+    
+    selectors.forEach((selector) => {
+        var target = document.querySelector('#' + selector);
+        if (target) {
+            if (properties.repeating && target.classList.contains('RepeatingGroup')) {
+                target.style.overflow = 'visible';
+                // Where repeating is enabled and target is a repeating group
+                for (const child of target.children) {
+                    setProps(child);
+                }
+            } else {
+            	setProps(target);
+            }
+        }
+    })
+    
+    AOS.refreshHard();
+    
+}
